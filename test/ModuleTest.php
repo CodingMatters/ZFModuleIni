8f443a0a08aaf94afcbc40290d6d0b5bdb024f9c
gabbydgab <gamba@gabbydgab.com>
2016-06-06 14:34:59 +0800

    updated config test

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -35,7 +35,7 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $module = new Module();
-        $this->config = $module->getConfig();        
+        $this->config = $module->getConfig();
     }
 
     /**
@@ -50,11 +50,14 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
     public function configKeys()
     {
         return [
-            'dependencies'          => ['dependencies'],
+            'service_manager'       => ['service_manager'],
             'templates'             => ['templates'],
             'view_manager'          => ['view_manager'],
             'middleware_pipeline'   => ['middleware_pipeline'],
-            'routes'                => ['routes']
+            'router'                => ['router'],
+            'view_helpers'          => ['view_helpers'],
+            'controllers'           => ['controllers'],
+            'controller_plugins'    => ['controller_plugins'],
         ];
     }
 }
